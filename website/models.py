bf132db0931f24c2ed537c2c6903898735e3fb85
Dreimos <reimosdrevon@gmail.com>
2023-09-10 12:37:04 +0300

    refactor - created classes for the autobase models

--- website/models.py
+++ website/models.py
@@ -1,21 +1,23 @@
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import text
-from sqlalchemy.engine.reflection import Inspector
 
 from . import db
 
 Base = automap_base()
+
+class UploadData(Base):
+    """
+    Relevant columns:
+    id - Primary Key
+    hash - magnet link UNIQUE
+    title - torrent name
+    cat - categories
+    size - upload size in bytes
+    imdb - imdb page (Nullable)
+    """
+    __tablename__ = 'upload_data'
+
+class Categories(Base):
+    __tablename__ = 'categories'
+
 Base.prepare(autoload_with=db.engine)
 
-
-"""
-Relevant columns:
-id - Primary Key
-hash - magnet link UNIQUE
-title - torrent name
-cat - categories
-size - upload size in bytes
-imdb - imdb page (Nullable)
-"""
-UploadData = Base.classes.upload_data
-Categories = Base.classes.categories
